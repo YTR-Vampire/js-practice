85b52f1b97a5d29773e989e1f964422287535f20
Yugel Tshering <yugel.tshering0@gmail.com>
2026-04-26 20:15:10 +0600

    Delete 1.5-celsius.fahrenheit.js
    Duplicate. Accidently pushed the entire directory instead of one file.

--- 1.5-celsius.fahrenheit.js
+++
@@ -1,7 +0,0 @@
-const celsiusToFahrenheit = (celsius) => {return celsius * 9/5 + 32;};
-const fahrenheitToCelsius = (fahrenheit) => {return (fahrenheit - 32) * 5/9;};      
-
-console.log(celsiusToFahrenheit(0));
-console.log(celsiusToFahrenheit(100));
-console.log(fahrenheitToCelsius(32));
-console.log(fahrenheitToCelsius(212));
