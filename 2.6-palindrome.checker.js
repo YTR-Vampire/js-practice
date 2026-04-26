56898b3e1dd71e7df077c15aa64ea152ee2e1004
Yugel Tshering <yugel.tshering0@gmail.com>
2026-04-26 20:17:24 +0600

    Delete 2.6-palindrome.checker.js
    Duplicate. Accidently pushed the entire directory instead of one file.

--- 2.6-palindrome.checker.js
+++
@@ -1,9 +0,0 @@
-const isPalindrome = (str) => {
-    const clear = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    return clear === [...clear].reverse().join('');
-};
-
-console.log(isPalindrome("madam"));
-console.log(isPalindrome("Racecar"));
-console.log(isPalindrome("hello"));
-console.log(isPalindrome("a"));
